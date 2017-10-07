2b1c0793a9c65ab9a92d6e376015716b924c77f6
Tiezzi96 <bernardo.tiezzi@stud.unifi.it>
2017-10-07 21:23:52 +0200

    class MaxFormula, files .h and .cpp

---
+++ ClionProjects/cellsumformula/MaxFormula.h
@@ -0,0 +1,53 @@
+//
+// Created by bernardo on 02/10/16.
+//
+
+#ifndef CELLSUMFORMULA_MAXFORMULA_H
+#define CELLSUMFORMULA_MAXFORMULA_H
+
+
+#include <iostream>
+#include "Observer.h"
+#include "Cell.h"
+
+class MaxFormula: public Observer {
+public:
+    MaxFormula(){}
+    virtual ~MaxFormula() { }
+    virtual void addCell(Cell* c){
+        cells.push_back(c);
+        //   c->addObserver(this);
+
+    }
+
+    virtual void removeCell(Cell* c){
+        cells.remove(c);
+        //    c->removeObserver(this);
+
+    }
+
+    virtual void update(){
+        _value=subject->getValue();
+    }
+
+    void calc(){
+        auto itr=begin(cells);
+        for(auto itr2=begin(cells);itr2!=end(cells); itr2++) {
+            if ((*itr)->getValue() < (*itr2)->getValue())
+                itr = itr2;
+        }
+        std::cout << "il valore piu grande è: " << (*itr)->getValue() << std::endl;
+    }
+
+private:
+    float _value;
+    std::list<Cell*>cells;
+    Cell* subject;
+
+};
+
+
+
+
+
+#endif //CELLSUMFORMULA_MAXFORMULA_H
