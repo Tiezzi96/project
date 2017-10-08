8421f8f432e92bf68d4a564ef53fbe021ac1cd0c
Tiezzi96 <bernardo.tiezzi@stud.unifi.it>
2017-10-08 17:40:21 +0200

    Class Observer, files .cpp and .h

---
+++ ClionProjects/cellsumformula/Observer.h
@@ -0,0 +1,20 @@
+//
+// Created by bernardo on 28/09/16.
+//
+
+#ifndef CELLSUMFORMULA_OBSERVER_H
+#define CELLSUMFORMULA_OBSERVER_H
+
+
+
+
+class Observer {
+public:
+    virtual void update()=0;
+
+
+
+};
+
+
+#endif //CELLSUMFORMULA_OBSERVER_H
