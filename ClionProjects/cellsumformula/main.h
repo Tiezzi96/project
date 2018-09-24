a1af84c469a47cad091e27d58b00ad9b7114eb0e
Tiezzi96 <32564725+Tiezzi96@users.noreply.github.com>
2018-09-24 09:15:14 +0200

    Add files via upload

---
+++ ClionProjects/cellsumformula/main.h
@@ -0,0 +1,17 @@
+//
+// Created by bernardo on 15/10/17.
+//
+
+#ifndef CELLSUMFORMULA_MAIN_H
+#define CELLSUMFORMULA_MAIN_H
+
+#include <wx/wx.h>
+
+class MyApp: public wxApp {
+public:
+    virtual bool OnInit();
+
+};
+
+
+#endif //CELLSUMFORMULA_MAIN_H
